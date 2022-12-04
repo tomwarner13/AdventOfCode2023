4352e487d8f2a2f6905bf57ec174ab1e232a3d12
Tom Warner <twarner@dealeron.com>
2022-12-04 18:34:58 -0500

    day 4

---
+++ AdventOfCode2022/DayFour/DayFourProblems.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.DayFour
+{
+  public static class DayFourProblems
+  {
+    public static int CalculateDuplicateAssignmentScore(IEnumerable<string> input)
+    {
+      var total = 0;
+      foreach (var line in input)
+      {
+        var parsed = ParseLine(line);
+        if (CheckIfRangeContained(parsed.first, parsed.second))
+        {
+          total += 1;
+        }
+      }
+
+      return total;
+    }
+
+    public static int CalculateOverlapAssignmentScoreFromInputFile(string filepath)
+    {
+      var input = File.ReadAllLines(filepath);
+      return CalculateOverlapAssignmentScore(input);
+    }
+
+    public static int CalculateOverlapAssignmentScore(IEnumerable<string> input)
+    {
+      var total = 0;
+      foreach (var line in input)
+      {
+        var parsed = ParseLine(line);
+        if (CheckIfAnyOverlap(parsed.first, parsed.second))
+        {
+          total += 1;
+        }
+      }
+
+      return total;
+    }
+
+    public static int CalculateDuplicateAssignmentScoreFromInputFile(string filepath)
+    {
+      var input = File.ReadAllLines(filepath);
+      return CalculateDuplicateAssignmentScore(input);
+    }
+
+    private static ((int, int) first, (int, int) second) ParseLine(string line)
+    {
+      var halves = line.Split(',');
+      return (ParseRange(halves[0]), ParseRange(halves[1]));
+    }
+
+    private static (int, int) ParseRange(string range)
+    {
+      var nums = range.Split('-');
+      return (int.Parse(nums[0]), int.Parse(nums[1]));
+    }
+
+    private static bool CheckIfRangeContained((int, int) first, (int, int) second)
+    {
+      return ((first.Item1 >= second.Item1) && (first.Item2 <= second.Item2)) ||
+             ((second.Item1 >= first.Item1) && (second.Item2 <= first.Item2));
+    }
+    private static bool CheckIfAnyOverlap((int, int) first, (int, int) second)
+    {
+      return ((first.Item1 >= second.Item1) && (first.Item1 <= second.Item2)) ||
+             ((second.Item1 >= first.Item1) && (second.Item1 <= first.Item2));
+    }
+  }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,22 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using AdventOfCode2022.DayThree;
+using AdventOfCode2022.DayFour;
 
 
-var input = @"vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw";
+var input = @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
 
 //Console.WriteLine(DayThreeProblems.CalculatePackPriorityScore(input.Split('\n')));
 //Console.WriteLine(DayThreeProblems.CalculatePackPriorityScoreFromInputFile("DayThree\\D3.txt"));
 
 
-Console.WriteLine(DayThreeProblems.CalculateBadgePriorityScore(input.Split('\n')));
-Console.WriteLine(DayThreeProblems.CalculateBadgePriorityScoreFromInputFile("DayThree\\D3.txt"));
+Console.WriteLine(DayFourProblems.CalculateDuplicateAssignmentScore(input.Split('\n')));
+Console.WriteLine(DayFourProblems.CalculateDuplicateAssignmentScoreFromInputFile("DayFour\\D4.txt"));
+
+
+Console.WriteLine(DayFourProblems.CalculateOverlapAssignmentScore(input.Split('\n')));
+Console.WriteLine(DayFourProblems.CalculateOverlapAssignmentScoreFromInputFile("DayFour\\D4.txt"));
 
 //Console.WriteLine(DayTwoProblems.CalculateRpsScoreWithInstructions(input.Split('\n')));
 //Console.WriteLine(DayTwoProblems.CalculateRpsScoreWithInstructionsFromInputFile("DayTwo\\D2.txt"));
