cde0bb420bf8dd41450274049c48d2e514877ee8
Tom Warner <twarner@dealeron.com>
2022-12-03 19:04:54 -0500

    add day 3 code

---
+++ AdventOfCode2022/DayThree/DayThreeProblems.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.DayThree
+{
+  public static class DayThreeProblems
+  {
+    public static int CalculatePackPriorityScore(IEnumerable<string> input)
+    {
+      var total = 0;
+      foreach (var line in input)
+      {
+        var (first, second) = SplitString(line);
+        var commonChar = FindCommonCharacter(first, second);
+        var value = GetCharValue(commonChar);
+        total += value;
+      }
+
+      return total;
+    }
+
+    public static int CalculatePackPriorityScoreFromInputFile(string filepath)
+    {
+      var input = File.ReadAllLines(filepath);
+      return CalculatePackPriorityScore(input);
+    }
+
+    public static int CalculateBadgePriorityScore(IEnumerable<string> input)
+    {
+      var total = 0;
+      var accumulator = new string[3];
+      var curLine = 0;
+
+      foreach (var line in input)
+      {
+        var mod = curLine % 3;
+        accumulator[mod] = line;
+
+        if (mod == 2)
+        {
+          //find common badge stuff and add to total
+          var commonChar = FindCommonCharBetweenLines(accumulator);
+          total += GetCharValue(commonChar);
+        }
+
+        curLine++;
+      }
+
+      return total;
+    }
+
+    public static int CalculateBadgePriorityScoreFromInputFile(string filepath)
+    {
+      var input = File.ReadAllLines(filepath);
+      return CalculateBadgePriorityScore(input);
+    }
+
+    private static char FindCommonCharBetweenLines(string[] lines)
+    {
+      var firstHash = lines[0].ToHashSet();
+      firstHash.IntersectWith(lines[1]);
+      firstHash.IntersectWith(lines[2]);
+
+      return firstHash.First();
+    }
+
+    private static (string firstHalf, string secondHalf) SplitString(string input)
+    {
+      var totalCount = input.Length;
+      var firstHalf = input[..(totalCount / 2)];
+      var secondHalf = input[(totalCount / 2)..];
+
+      return (firstHalf, secondHalf);
+    }
+
+    private static char FindCommonCharacter(string firstHalf, string secondHalf)
+    {
+      var firstHash = firstHalf.ToHashSet();
+      firstHash.IntersectWith(secondHalf);
+      return firstHash.First();
+    }
+
+    private static int GetCharValue(char c)
+    {
+      var i = (int)c;
+      if (i < 91) //uppercase
+        return i - 38;
+      return i - 96; //lowercase
+    }
+  }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,35 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using AdventOfCode2022.DayOne;
-using AdventOfCode2022.DayTwo;
-
-//var inputBasic = @"1000
-//2000
-//3000
-
-//4000
-
-//5000
-//6000
-
-//7000
-//8000
-//9000
-
-//10000";
+using AdventOfCode2022.DayThree;
 
 
-//Console.WriteLine(DayOneProblems.CalculateTopNCalories(inputBasic.Split('\n'), 3));
-//Console.WriteLine(DayOneProblems.CalculateTopNCaloriesFromInputFile("DayOne\\D1P1.txt", 3));
+var input = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
 
-var input = @"A Y
-B X
-C Z";
-
-Console.WriteLine(DayTwoProblems.CalculateRpsScore(input.Split('\n')));
-Console.WriteLine(DayTwoProblems.CalculateRpsScoreFromInputFile("DayTwo\\D2.txt"));
+//Console.WriteLine(DayThreeProblems.CalculatePackPriorityScore(input.Split('\n')));
+//Console.WriteLine(DayThreeProblems.CalculatePackPriorityScoreFromInputFile("DayThree\\D3.txt"));
 
 
-Console.WriteLine(DayTwoProblems.CalculateRpsScoreWithInstructions(input.Split('\n')));
-Console.WriteLine(DayTwoProblems.CalculateRpsScoreWithInstructionsFromInputFile("DayTwo\\D2.txt"));
+Console.WriteLine(DayThreeProblems.CalculateBadgePriorityScore(input.Split('\n')));
+Console.WriteLine(DayThreeProblems.CalculateBadgePriorityScoreFromInputFile("DayThree\\D3.txt"));
+
+//Console.WriteLine(DayTwoProblems.CalculateRpsScoreWithInstructions(input.Split('\n')));
+//Console.WriteLine(DayTwoProblems.CalculateRpsScoreWithInstructionsFromInputFile("DayTwo\\D2.txt"));
