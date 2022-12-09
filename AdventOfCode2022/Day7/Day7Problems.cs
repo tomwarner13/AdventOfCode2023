d9ce236bd709d74038b206dfe05624fb85bdcfa7
Tom Warner <twarner@dealeron.com>
2022-12-09 14:20:17 -0500

    complete day 7

--- AdventOfCode2022/Day7/Day7Problems.cs
+++ AdventOfCode2022/Day7/Day7Problems.cs
@@ -41,16 +41,190 @@ $ ls
 
     public override string Problem1(string[] input)
     {
+      var rootDirectory = new Directory((Directory)null, "/");
+      var currentDirectory = rootDirectory;
+      var allDirectories = new List<Directory> { rootDirectory };
 
+      foreach (var line in input.Select(l => l.Trim()))
+      {
+        if (line.StartsWith("$ "))
+        {
+          //handle a command -- either CD to root directory, CD to current parent directory, or CD to child directory
+          var command = line.Substring(2);
+          if (command.StartsWith("cd"))
+          {
+            var target = command.Substring(3);
+            switch (target)
+            {
+              case "/":
+                currentDirectory = rootDirectory;
+                break;
+              case "..":
+                currentDirectory = currentDirectory.Parent;
+                break;
+              default:
+                currentDirectory = currentDirectory.OpenChildDirectory(target);
+                break;
+            }
+          }
+          else //ls, do nothing
+          {
 
-      throw new NotImplementedException();
+          }
+        }
+        else
+        {
+          //handle a new item -- either "dir <dirname>" or "<filesize> <filename>"
+          if (line.StartsWith("dir"))
+          {
+            var newDirName = line.Substring(4);
+            var newDir = new Directory(currentDirectory, newDirName);
+            allDirectories.Add(newDir);
+            currentDirectory.AddChild(newDir);
+          }
+          else
+          {
+            var parts = line.Split(' ');
+            var size = int.Parse(parts[0]);
+            var name = parts[1];
+            currentDirectory.AddChild(new File(currentDirectory, name, size));
+          }
+        }
+      }
+
+      //grotesquely inefficient but processing is cheap
+      var countableDirectories = allDirectories.Where(d => d.CalculateSize() <= MaxDirSizeToCount);
+
+      var total = countableDirectories.Select(d => d.CalculateSize()).Sum();
+
+      return total.ToString();
     }
 
     public override string Problem2(string[] input)
     {
+      var rootDirectory = new Directory((Directory)null, "/");
+      var currentDirectory = rootDirectory;
+      var allDirectories = new List<Directory> { rootDirectory };
 
+      foreach (var line in input.Select(l => l.Trim()))
+      {
+        if (line.StartsWith("$ "))
+        {
+          //handle a command -- either CD to root directory, CD to current parent directory, or CD to child directory
+          var command = line.Substring(2);
+          if (command.StartsWith("cd"))
+          {
+            var target = command.Substring(3);
+            switch (target)
+            {
+              case "/":
+                currentDirectory = rootDirectory;
+                break;
+              case "..":
+                currentDirectory = currentDirectory.Parent;
+                break;
+              default:
+                currentDirectory = currentDirectory.OpenChildDirectory(target);
+                break;
+            }
+          }
+          else //ls, do nothing
+          {
 
-      throw new NotImplementedException();
+          }
+        }
+        else
+        {
+          //handle a new item -- either "dir <dirname>" or "<filesize> <filename>"
+          if (line.StartsWith("dir"))
+          {
+            var newDirName = line.Substring(4);
+            var newDir = new Directory(currentDirectory, newDirName);
+            allDirectories.Add(newDir);
+            currentDirectory.AddChild(newDir);
+          }
+          else
+          {
+            var parts = line.Split(' ');
+            var size = int.Parse(parts[0]);
+            var name = parts[1];
+            currentDirectory.AddChild(new File(currentDirectory, name, size));
+          }
+        }
+      }
+
+      var totalFileSpace = 70000000;
+      var requiredSpace = 30000000;
+
+      var totalSpaceTaken = rootDirectory.CalculateSize();
+      var totalSpaceAvailableForSystem = totalFileSpace - requiredSpace;
+
+      var totalSpaceRequiredToBeFreed = totalSpaceTaken - totalSpaceAvailableForSystem;
+
+      var directoryToDelete = rootDirectory;
+      var currentSizeToDelete = totalSpaceTaken;
+      foreach (var dir in allDirectories)
+      {
+        var dirSize = dir.CalculateSize();
+        if (dirSize > totalSpaceRequiredToBeFreed && dirSize < currentSizeToDelete)
+        {
+          directoryToDelete = dir;
+          currentSizeToDelete = dirSize;
+        }
+      }
+
+      return currentSizeToDelete.ToString();
+    }
+
+    private abstract class Item
+    {
+      public Directory Parent;
+      public string Name;
+
+      public Item(Directory parent, string name)
+      {
+        Parent = parent;
+        Name = name;
+      }
+
+      public abstract int CalculateSize();
+    }
+
+    private class Directory : Item
+    {
+      public List<Item> Children;
+
+      public Directory(Directory parent, string name) : base(parent, name)
+      {
+        Children = new List<Item>();
+      }
+
+      public void AddChild(Item item)
+      {
+        Children.Add(item);
+      }
+
+      public override int CalculateSize()
+      {
+        return Children.Select(i => i.CalculateSize()).Sum();
+      }
+
+      public Directory OpenChildDirectory(string name)
+      {
+        return (Directory)Children.First(c => c.Name == name);
+      }
+    }
+
+    private class File : Item
+    {
+      private readonly int Size;
+
+      public File(Directory parent, string name, int size) : base(parent, name)
+      {
+        Size = size;
+      }
+
+      public override int CalculateSize() => Size;
     }
   }
 }
