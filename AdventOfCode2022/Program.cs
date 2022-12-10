ceb53815f53ad6dd259e93bf58854b3369202d75
Tom Warner <twarner@dealeron.com>
2022-12-10 10:32:25 -0500

    day 9

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using AdventOfCode2022.Day8;
+using AdventOfCode2022.Day9;
 using AdventOfCode2022.Util;
 
 
-var problems = new Day8Problems();
+var problems = new Day9Problems();
 DoAllProblems(problems);
 
 
