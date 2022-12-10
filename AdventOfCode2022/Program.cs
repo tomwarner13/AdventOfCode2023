b04f02aedf0c695d53cffaf04f9364c309f6b6ce
Tom Warner <tomwarner13@gmail.com>
2022-12-10 11:11:21 -0500

    day 10

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using AdventOfCode2022.Day9;
+using AdventOfCode2022.Day10;
 using AdventOfCode2022.Util;
 
 
-var problems = new Day9Problems();
+var problems = new Day10Problems();
 DoAllProblems(problems);
 
 
