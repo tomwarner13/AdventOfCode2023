4c3fb89fdc5609d4e9199d365f55d99b5db62975
Tom Warner <tomwarner13@gmail.com>
2022-12-14 21:17:34 -0500

    day 14

---
+++ AdventOfCode2022/Util/GridPoint.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Util
+{
+  public struct GridPoint : IEquatable<GridPoint>
+  {
+    public readonly int X;
+    public readonly int Y;
+
+    public GridPoint(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+
+    public bool Equals(GridPoint other)
+    {
+      return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+      return obj is GridPoint other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+      return HashCode.Combine(X, Y);
+    }
+
+    public static bool operator ==(GridPoint a, GridPoint b)
+    {
+      return a.Equals(b);
+    }
+
+    public static bool operator !=(GridPoint a, GridPoint b)
+    {
+      return !a.Equals(b);
+    }
+
+    public override string ToString() => $"{X}:{Y}";
+  }
+}
