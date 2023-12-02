b9111dc67fa587159605432e4ffb9fb2bf94e15d
Tom Warner <tomwarner13@gmail.com>
2023-12-02 10:39:16 -0500

    Day 1

--- AdventOfCode2023/Day1/Day1Problems.cs
+++ AdventOfCode2023/Day1/Day1Problems.cs
@@ -1,84 +1,72 @@
-﻿using AdventOfCode2023.Util;
+﻿using System.Text.RegularExpressions;
+using AdventOfCode2023.Util;
 
 namespace AdventOfCode2023.Day1;
 
 public class Day1Problems : Problems
 {
-  public override string TestInput => @"1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
+  public override string TestInput => @"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
 
   public override int Day => 1;
 
   public override string Problem1(string[] input, bool isTestInput)
   {
-    return CalculateMostCalories(input).ToString();
+    return CalculateDigitSum(input).ToString();
   }
 
   public override string Problem2(string[] input, bool isTestInput)
   {
-    return CalculateTopNCalories(input, 3).ToString();
+    return CalculateDigitSumWithLetters(input).ToString();
   }
 
-  public static int CalculateMostCalories(IEnumerable<string> input)
+  private static int CalculateDigitSum(IEnumerable<string> input)
   {
-    var maxCalories = 0;
-    var curCalories = 0;
-
-    foreach (var line in input)
-    {
-      if (string.IsNullOrWhiteSpace(line))
-      {
-        if (curCalories > maxCalories)
-          maxCalories = curCalories;
-        curCalories = 0;
-      }
-      else
-      {
-        var lineItemCalories = int.Parse(line);
-        curCalories += lineItemCalories;
-      }
-    }
-
-    if(curCalories > 0 && curCalories > maxCalories)
-      maxCalories = curCalories;
-
-    return maxCalories;
+    return input.Sum(ParseDigitsFromString);
   }
 
-  public static int CalculateTopNCalories(IEnumerable<string> input, int topNumber)
+  private static int ParseDigitsFromString(string inputLine)
   {
-    var allCalories = new List<int>();
-    var curCalories = 0;
+    var digitRegex = new Regex("\\d");
+    var digits = digitRegex.Matches(inputLine).ToArray();
+    var firstDigit = digits.First().ToString()[0];
+    var lastDigit = digits.Last().ToString()[0];
+    var foundNumber = $"{firstDigit}{lastDigit}";
+    return int.Parse(foundNumber);
+  }
+  
+  public static int CalculateDigitSumWithLetters(IEnumerable<string> input)
+  {
+    return input.Sum(ConvertToDigitsWithLetters);
+  }
+  
+  private static int ConvertToDigitsWithLetters(string inputLine)
+  {
+    var digitRegex = new Regex("(?=(\\d|one|two|three|four|five|six|seven|eight|nine))");
+    var digits = digitRegex.Matches(inputLine).ToArray();
+    var firstDigit = PreprocessDigitString(digits.First().Groups[1].ToString());
+    var lastDigit = PreprocessDigitString(digits.Last().Groups[1].ToString());
+    var foundNumber = $"{firstDigit}{lastDigit}";
+    return int.Parse(foundNumber);
+  }
 
-    foreach (var line in input)
-    {
-      if (string.IsNullOrWhiteSpace(line))
-      {
-        allCalories.Add(curCalories);
-        curCalories = 0;
-      }
-      else
-      {
-        var lineItemCalories = int.Parse(line);
-        curCalories += lineItemCalories;
-      }
-    }
-
-    if(curCalories > 0)
-      allCalories.Add(curCalories);
-
-    return allCalories.OrderByDescending(x => x).Take(topNumber).Sum();
+  private static string PreprocessDigitString(string input)
+  {
+    if (input.Length == 1) return input;
+    
+    return input.Replace("one", "1")
+      .Replace("two", "2")
+      .Replace("three", "3")
+      .Replace("four", "4")
+      .Replace("five", "5")
+      .Replace("six", "6")
+      .Replace("seven", "7")
+      .Replace("eight", "8")
+      .Replace("nine", "9");
   }
 }
